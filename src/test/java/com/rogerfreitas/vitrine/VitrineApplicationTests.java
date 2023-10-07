11cb302cd1ab08cda457d6145090f356fd768822
Roger <roger.freitas@supera.com.br>
2023-10-07 00:55:11 -0300

    configuraçoes iniciais e estrutura inicial do projeto

---
+++ src/test/java/com/rogerfreitas/vitrine/VitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerfreitas.vitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
